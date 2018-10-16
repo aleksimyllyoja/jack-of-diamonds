b2f9a9aeef025304bb95f0b29015f18dd949932b
Aleksi Myllyoja <aleksi.myllyoja@gmail.com>
2018-10-16 21:29:06 +0300

    more parameters to example3

--- example3.js
+++ example3.js
@@ -7,7 +7,9 @@ function example3() {
     xphase: 0,
     yphase: 0,
     phase: 0,
-    random: 100
+    random: 100,
+    n: 3,
+    bp: 10
   }
 
   var c1sf = gui.addFolder("Circle 1");
@@ -19,6 +21,8 @@ function example3() {
   c1sf.add(c1s, 'xphase', -7, 7).onChange(draw);
   c1sf.add(c1s, 'yphase', -7, 7).onChange(draw);
   c1sf.add(c1s, 'random', 0, 1000).onChange(draw);
+  c1sf.add(c1s, 'n', 3, 10).onChange(draw);
+  c1sf.add(c1s, 'bp', 3, 100).onChange(draw);
   c1sf.open();
 
   function random_bezier() {
@@ -32,7 +36,7 @@ function example3() {
       c1s.phase, c1s.xphase, c1s.yphase, c1s.rx, c1s.ry
     );
 
-    b = bezier(_.sample(c, 3), 10);
+    b = bezier(_.sample(c, c1s.n), c1s.bp);
     return [b];
   }
 }

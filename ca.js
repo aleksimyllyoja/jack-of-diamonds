bb3e6b4d1ad132ff9558427b1eba435eba985128
Aleksi Myllyoja <aleksi.myllyoja@gmail.com>
2018-10-28 00:15:31 +0300

    fractal thing

---
+++ ca.js
@@ -0,0 +1,78 @@
+function ca() {
+  S = {
+    iterations: 1,
+    amplitude: 0.3,
+    phase: 0,
+    phasex: 0,
+    phasey: 0,
+    radius: 20,
+    sp: 3,
+    c: 10,
+    cp: 10,
+    bezier_precision: 4,
+  }
+
+  var g = gui.addFolder("Variables");
+  g.add(S, 'iterations', 0, 5, 1).onChange(draw);
+  g.add(S, 'amplitude', 0, 10).onChange(draw);
+  g.add(S, 'radius', 1, 100).onChange(draw);
+  g.add(S, 'bezier_precision', 2, 20, 1).onChange(draw);
+  g.add(S, 'sp', 2, 20, 1).onChange(draw);
+  g.add(S, 'c', 0, 20, 1).onChange(draw);
+  g.add(S, 'cp', 1, 100).onChange(draw);
+
+
+  g.add(S, 'phasex', -Math.PI*2, Math.PI*2).onChange(draw);
+  g.add(S, 'phasey', -Math.PI*2, Math.PI*2).onChange(draw);
+  g.add(S, 'phase', -Math.PI*2, Math.PI*2).onChange(draw);
+
+  g.open();
+
+  function ln(x1, y1, x2, y2) {
+    return Math.sqrt((x1-x2)**2+(y1-y2)**2);
+  }
+
+  function l(x1, y1, x2, y2, d) {
+    d = d ? d : 1;
+    var mx = (x1+x2)*0.5;
+    var my = (y1+y2)*0.5;
+    var a = Math.atan2(y2, x2) - Math.atan2(y1, x1);
+    var m = S.amplitude*ln(x1, y1, x2, y2)
+
+    var vx = mx+d*m*Math.cos(a);
+    var vy = my+d*m*Math.sin(a);
+
+
+    var l1 = bezier([
+      [x1, y1],
+      [vx, vy],
+      [x2, y2]
+    ], S.bezier_precision)
+    return l1;
+  }
+
+  function _draw(i) {
+    var ps = circle(W/2, H/2, S.radius, S.sp, S.phase+i/S.cp, S.phasex, S.phasey);
+
+    for(var i=0;i<S.iterations; i++) {
+      ps = iter(i);
+    }
+    function iter(i) {
+      var nps = [];
+      for(var i=0; i<ps.length-1; i++) {
+        var l2 = l(ps[i][0], ps[i][1], ps[i+1][0], ps[i+1][1], (-1)**i);
+        nps.push.apply(nps, l2)
+      }
+      return nps;
+    }
+
+    return [ps];
+  }
+
+  return function(j, max) {
+    var ps = [];
+    for(var i=0; i<S.c; i++) ps.push.apply(ps, _draw(i));
+    return ps;
+  }
+}
+register("ca", ca);
